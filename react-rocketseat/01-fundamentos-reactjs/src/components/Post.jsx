490725e2f6470d914f9f2a192f1f460a5e66aad4
lucasfreitasr <lucasfreitasr@outlook.com>
2022-07-01 12:57:24 -0300

    Continuando a estilização dos comentarios

--- react-rocketseat/01-fundamentos-reactjs/src/components/Post.jsx
+++ react-rocketseat/01-fundamentos-reactjs/src/components/Post.jsx
@@ -1,15 +1,15 @@
 import styles from './Post.module.css'
 
-export function Post(){
-    return(
+export function Post() {
+    return (
         <article className={styles.Post}>
             <header>
                 <div className={styles.author}>
-                <img  className={styles.avatar} src="https://avatars.githubusercontent.com/u/54214156?v=4" alt="" />
-                <div className={styles.authorInfo}>
-                    <strong>Lucas Freitas</strong>
-                    <span>React Developer</span>
-                </div>
+                    <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/54214156?v=4" alt="" />
+                    <div className={styles.authorInfo}>
+                        <strong>Lucas Freitas</strong>
+                        <span>React Developer</span>
+                    </div>
                 </div>
                 <time title="29 de Junho as 22:30" dateTime="2022-06-29 22:30">
                     Publicado há uma 1h
@@ -18,7 +18,7 @@ export function Post(){
 
 
             <div className={styles.content}>
-                
+
                 <p>Fala galeraa 👋</p>
 
                 <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
@@ -26,20 +26,22 @@ export function Post(){
                 <p>
                     {' '}<a href=''>jane.design/doctorcare</a></p>
 
-                <p> <a href=''>#novoprojeto</a>{' '} 
-                <a href=''>#nlw</a>{' '}
-                <a href=''>#rocketseat</a></p>
+                <p> <a href=''>#novoprojeto</a>{' '}
+                    <a href=''>#nlw</a>{' '}
+                    <a href=''>#rocketseat</a></p>
 
             </div>
 
             <form className={styles.commentForm}>
                 <strong>Deixe seu feedback</strong>
 
-                <textarea 
-                   placeholder='Deixe seu comentário'
-                />  
+                <textarea
+                    placeholder='Deixe seu comentário'
+                />
+                <footer>
+                    <button type='submit'>Comentar</button>
 
-                <button type='submit'>Comentar</button>
+                </footer>
             </form>
         </article>
     )
