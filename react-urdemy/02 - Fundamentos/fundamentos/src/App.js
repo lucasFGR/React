f9fe783e78349bfd78d333eb6e58c6ae9cba4a42
lucasFGR <lucasfreitasr@outlook.com>
2022-07-07 23:54:41 -0300

    Entrando na parte de fundamentos

---
+++ react-urdemy/02 - Fundamentos/fundamentos/src/App.js
@@ -0,0 +1,20 @@
+//Components
+
+import FirstComponent from './components/FirstComponent';
+
+
+// Style / CSS
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>02 -Fundamentos</h1>
+
+      <FirstComponent/>
+    </div>
+  );
+}
+
+export default App;
