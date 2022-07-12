b55c27f323d3b1677c6ef1277a284ebcf56ca43b
Lucas Gonçalves <lucasg@emphasisconf.com.br>
2022-07-12 07:58:46 -0300

    a

--- react-rocketseat/01-fundamentos-reactjs/src/components/Avatar.jsx
+++ react-rocketseat/01-fundamentos-reactjs/src/components/Avatar.jsx
@@ -1,10 +1,9 @@
 import styles from './Avatar.module.css'
-export function Avatar (props){
+export function Avatar ({hasBorder = true,src}){
 
-    const hasBorder = props.hasBorder !== false
 
     return  (
         <img  className={hasBorder ? styles.avatarWithBorder: styles.avatar} 
-        src={props.src}/>
+        src={src}/>
     )
 }
