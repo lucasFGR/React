5c471a5fbaa92120921c69038e2619dddc20d552
lucasfreitasr@outlook.com.br <lucasfreitasr@outlook.com.br>
2022-07-13 23:58:28 -0300

    Estilizando componentes

--- react-udemy/04 - CSS E REACT/css/src/App.js
+++ react-udemy/04 - CSS E REACT/css/src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import MyComponent from './components/MyComponent';
 
 function App() {
   return (
     <div className="App">
      <h1>React com CSS</h1>
+     <MyComponent/>
+     <p>ESte paragrafo é do App.js</p>
     </div>
   );
 }
---
+++ react-udemy/04 - CSS E REACT/css/src/components/MyComponent.js
@@ -0,0 +1,13 @@
+import "./MyComponent.css"
+
+const MyComponent = ()=>{
+    return (
+        <div>
+            <h1>Meu CSS</h1>
+            <p>ESte é o paragrafo do componente</p>
+            <p className="my-comp-paragraph">Este tbm é um paragrafo do componente</p>
+        </div>
+    )
+}
+
+export default MyComponent
